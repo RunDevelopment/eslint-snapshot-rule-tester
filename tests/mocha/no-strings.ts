64041d36af13b31eeafa225f17f27bef19c1cce4
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-03-18 13:52:03 +0100

    No strings test

---
+++ tests/mocha/no-strings.ts
@@ -0,0 +1,79 @@
+import { Rule } from "eslint"
+import { SnapshotRuleTester } from "../../src/rule-tester"
+
+const rule: Rule.RuleModule = {
+    meta: {
+        fixable: "code",
+        hasSuggestions: true,
+        type: "suggestion",
+        messages: {
+            string: "Strings are not allowed. Use a number instead.",
+            template: "Template strings are not allowed.",
+            somethingElse: "Use something else instead.",
+        },
+    },
+    schema: [
+        {
+            type: "object",
+            properties: {
+                somethingElse: {
+                    type: "string",
+                },
+            },
+            additionalProperties: false,
+        },
+    ],
+    create(context) {
+        const somethingElse = context.options[0]?.somethingElse ?? "somethingElse"
+
+        return {
+            Literal(node) {
+                if (typeof node.value === "string") {
+                    context.report({
+                        node,
+                        messageId: "string",
+                        fix(fixer) {
+                            return fixer.replaceText(node, "42")
+                        },
+                    })
+                }
+            },
+            TemplateLiteral(node) {
+                context.report({
+                    node,
+                    messageId: "template",
+                    suggest: [
+                        {
+                            messageId: "somethingElse",
+                            fix(fixer) {
+                                return fixer.replaceText(node, somethingElse)
+                            },
+                        },
+                    ],
+                })
+            },
+        }
+    },
+}
+
+// Example
+
+const tester = new SnapshotRuleTester()
+
+tester.run("no-strings", rule, {
+    valid: [String.raw`1 + 2 + 3`],
+    invalid: [
+        `const a = ["foo", "bar"]${"\n"}const b = a.join("\\n")`,
+        String.raw`class Foo {
+    foo = "foo"
+    constructor() {
+        this.bar = "bar"
+    }
+}`,
+        String.raw`["foo", "bar"].join("\n")`,
+        {
+            code: '`foo ${a ? "bar" : "baz"} ${`_${""}_`}`',
+            options: [{ somethingElse: "REPLACEMENT" }],
+        },
+    ],
+})
