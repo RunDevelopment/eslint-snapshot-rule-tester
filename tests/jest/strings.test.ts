ef53eefec8ae90a7f51f48b1a52d76dca1840242
Michael Schmidt <msrd0000@gmail.com>
2024-03-18 19:19:27 +0100

    Add support for Jest (#4)

---
+++ tests/jest/strings.test.ts
@@ -0,0 +1,94 @@
+import { Rule } from "eslint"
+import { SnapshotRuleTester } from "../../src/rule-tester"
+
+const rule: Rule.RuleModule = {
+    meta: {
+        fixable: "code",
+        hasSuggestions: true,
+        type: "suggestion",
+        messages: {
+            string: "Found a string. Use `{{expr}}` instead.",
+            template: "Found a template string. Use `{{expr}}` instead.",
+            somethingElse: "Use something else instead.",
+        },
+    },
+    schema: [],
+    create(context) {
+        return {
+            Literal(node) {
+                if (typeof node.value === "string") {
+                    context.report({
+                        node,
+                        messageId: "string",
+                        data: { expr: "foo" },
+                        fix(fixer) {
+                            return fixer.replaceText(node, "foo")
+                        },
+                    })
+                }
+            },
+            TemplateLiteral(node) {
+                context.report({
+                    node,
+                    messageId: "template",
+                    data: { expr: "bar" },
+                    suggest: [
+                        {
+                            messageId: "somethingElse",
+                            fix(fixer) {
+                                return fixer.replaceText(node, "somethingElse")
+                            },
+                        },
+                    ],
+                })
+            },
+        }
+    },
+}
+
+const tester = new SnapshotRuleTester({
+    languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+})
+
+tester.run("strings", rule, {
+    valid: [
+        String.raw`1 + 2`,
+        String.raw`
+class Foo {
+    constructor() {
+        this.bar = 12
+    }
+}
+`.trim(),
+    ],
+    invalid: [
+        String.raw`1 + "foo"`,
+        String.raw`"foo" + 1`,
+        String.raw`"foo" + 1${"\n"}`,
+        String.raw`"foo" + 1${"\r\n"}`,
+        String.raw`"foo"${"\t"}+ 1`,
+        {
+            code: String.raw`1 + "foo"`,
+            filename: "foo.js",
+        },
+        {
+            code: String.raw`1 + "foo"`,
+            name: "this is a custom name",
+        },
+        String.raw`
+class Foo {
+    foo = "foo"
+    constructor() {
+        this.bar = "bar"
+    }
+}
+`.trim(),
+        String.raw`["foo", "bar"].join("\n")`,
+        '`foo ${a ? "bar" : "baz"} ${`_${""}_`}`',
+        '`\nfoo ${a ? "bar" : "baz"}\n${`_${""}_`}\n` + "1"+"2"',
+        "`foo${1 + 2}`",
+    ],
+})
